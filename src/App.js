0810ff18535afffcee9700f20ec83f51154ca58b
Wojtek <vopox07@gmail.com>
2020-10-11 10:28:41 +0200

    Install bootstrap. Add icon. Add basic home page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from 'react-bootstrap';
+import Credits from './components/home/credits';
+import './style/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='parent d-flex flex-column justify-content-between'>
+      <Navbar className='d-flex flex-row justify-content-between align-items-center'>
+        <div className='big-font pr-2'>Welcome!{' '}</div>
+        <div className='pb-1'>Sign in to continue or sign up if you didn't already.</div>
+      </Navbar>
+      <div className='d-flex flex-row justify-content-around'>
+        <div className='big-font mid-element bg-highlight d-flex flex-column justify-content-around'>
+          <div>Login</div>
+          <div>email</div>
+          <div>password</div>
+        </div>
+        <div className='big-font mid-element bg-highlight d-flex flex-column justify-content-around'>
+          <div>Register</div>
+          <div>email</div>
+          <div>password</div>
+          <div>confirm password</div>
+        </div>
+      </div>
+      <div className=''>
+        <Credits />
+      </div>
     </div>
   );
 }
---
+++ src/components/home/credits.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Credits = (className) => {
+  return (
+    <div>
+      Credits: author Wojciech Kud
+      <br />
+      website icon made by{' '}
+      <a className='link' href='https://www.flaticon.com/authors/dinosoftlabs'>
+        DinosoftLabs
+      </a>{' '}
+      from{' '}
+      <a className='link' href='https://www.flaticon.com/'>
+        www.flaticon.com
+      </a>
+    </div>
+  );
+};
+
+export default Credits;

1ae9b48ae706d950b3442b8850f98ea4f4040d01
Wojtek <vopox07@gmail.com>
2020-10-11 17:04:35 +0200

    Set homepage layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container, Row, Col, Button } from 'react-bootstrap';
 import Credits from './components/home/credits';
 import './style/App.css';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
-    <div className='parent d-flex flex-column justify-content-between'>
+    <div className='parent d-flex flex-column justify-content-between '>
       <Navbar className='d-flex flex-row justify-content-between align-items-center'>
-        <div className='big-font pr-2'>Welcome!{' '}</div>
-        <div className='pb-1'>Sign in to continue or sign up if you didn't already.</div>
+        <div className='big-font pr-2'>Welcome! </div>
+        <div className='pb-1'>
+          Sign in to continue or sign up if you didn't already.
+        </div>
       </Navbar>
-      <div className='d-flex flex-row justify-content-around'>
-        <div className='big-font mid-element bg-highlight d-flex flex-column justify-content-around'>
-          <div>Login</div>
-          <div>email</div>
-          <div>password</div>
-        </div>
-        <div className='big-font mid-element bg-highlight d-flex flex-column justify-content-around'>
-          <div>Register</div>
-          <div>email</div>
-          <div>password</div>
-          <div>confirm password</div>
-        </div>
-      </div>
-      <div className=''>
-        <Credits />
-      </div>
+      <Container fluid>
+        <Row className='big-font'>
+          <Col xs />
+          <Col
+            className='bg-highlight d-flex flex-column justify-content-between'
+            xs={4}
+          >
+            <div>
+              <Row>
+                <Col>Login</Col>
+              </Row>
+              <Row>
+                <Col>email</Col>
+              </Row>
+              <Row>
+                <Col>password</Col>
+              </Row>
+            </div>
+            <div>
+              <Row>
+                <Col xs={8} />
+                <Col xs={3}>ok</Col>
+              </Row>
+            </div>
+          </Col>
+          <Col xs />
+          <Col className='bg-highlight' xs={4}>
+            <Row>
+              <Col>Register</Col>
+            </Row>
+            <Row>
+              <Col>email</Col>
+            </Row>
+            <Row>
+              <Col>password</Col>
+            </Row>
+            <Row>
+              <Col>confirm password</Col>
+            </Row>
+            <Row>
+              <Col xs={8} />
+              <Col xs={3}>ok</Col>
+            </Row>
+          </Col>
+          <Col xs />
+        </Row>
+      </Container>
+      <Credits />
     </div>
   );
 }

1f526bb123084e58721d9294f6d9e6c40489cf23
Wojtek <vopox07@gmail.com>
2020-10-30 17:29:38 +0100

    Fix bad use of useState hook

--- src/components/pages/ExpenseList.js
+++ src/components/pages/ExpenseList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { ListGroup, Button } from 'react-bootstrap';
 import { ExpenseContext } from '../../context/ExpenseContext';
 import Expense from '../expenses/Expense';
@@ -6,154 +6,12 @@ import ModelListHeader from '../common-for-models/ModelListHeader';
 
 const ExpenseList = () => {
   const [expenses, { getAll }] = useContext(ExpenseContext);
-  const [loaded, setLoaded] = useState(false);
 
   const getExpenses = () => {
-    if (!loaded && expenses.list.length === 0) {
+    if (expenses.list.length === 0)
       getAll();
-      setLoaded(true);
-    }
-    // return expenses.list; //TODO delete dummy data
-    return [
-      {
-        id: 3,
-        product_name: 'bread',
-        price: 2,
-        amount: 2,
-        date: '2020-11-01',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 4,
-        product_name: 'bread',
-        price: 2,
-        amount: 2,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 1,
-      },
-      {
-        id: 5,
-        product_name: 'bread',
-        price: 2,
-        amount: 2,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 1,
-      },
-      {
-        id: 7,
-        product_name: 'bread',
-        price: 3,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 8,
-        product_name: 'milk',
-        price: 3,
-        amount: 6,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 9,
-        product_name: 'milk',
-        price: 3,
-        amount: 6,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 10,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 11,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 12,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 13,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 14,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 15,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 16,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 17,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-      {
-        id: 18,
-        product_name: 'shopping',
-        price: 60,
-        amount: 1,
-        date: '2020-10-26',
-        user_id: 3,
-        category_id: 3,
-      },
-    ];
+    return expenses.list
   };
-
-
   return (
     <div className='d-flex flex-column align-items-center'>
         <ModelListHeader syncAction={getAll} headerCaption='Expenses' />
--- src/context/ExpenseContext.js
+++ src/context/ExpenseContext.js
@@ -1,6 +1,144 @@
 import React, { createContext, useState } from 'react';
 import inzApi from '../apis/inzApi';
 
+const dummyExpenses = [
+  {
+    id: 3,
+    product_name: 'bread',
+    price: 2,
+    amount: 2,
+    date: '2020-11-01',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 4,
+    product_name: 'bread',
+    price: 2,
+    amount: 2,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 1,
+  },
+  {
+    id: 5,
+    product_name: 'bread',
+    price: 2,
+    amount: 2,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 1,
+  },
+  {
+    id: 7,
+    product_name: 'bread',
+    price: 3,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 8,
+    product_name: 'milk',
+    price: 3,
+    amount: 6,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 9,
+    product_name: 'milk',
+    price: 3,
+    amount: 6,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 10,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 11,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 12,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 13,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 14,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 15,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 16,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 17,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+  {
+    id: 18,
+    product_name: 'shopping',
+    price: 60,
+    amount: 1,
+    date: '2020-10-26',
+    user_id: 3,
+    category_id: 3,
+  },
+];
+
 export const ExpenseContext = createContext();
 
 export const ExpenseProvider = (props) => {
@@ -10,24 +148,28 @@ export const ExpenseProvider = (props) => {
   });
 
   const getAll = () => {
-    setExpenses({ ...expenses, loading: true });
+    if (expenses.loading)
+      return 
 
-    inzApi()
-      .get('expense')
-      .then((res) => {
-        setExpenses({
-          ...expenses,
-          loading: false,
-          list: res.data.sort(
-            (e1, e2) => new Date(e2.date) - new Date(e1.date)
-          ),
-        });
-      })
-      .catch((err) => {
-        //TODO catch backend errors
-        console.log(err);
-        setExpenses({ ...expenses, loading: false });
-      });
+      setExpenses({...expenses, list: dummyExpenses})
+    // setExpenses({ ...expenses, loading: true });
+    //
+    // inzApi()
+    //   .get('expense')
+    //   .then((res) => {
+    //     setExpenses({
+    //       ...expenses,
+    //       loading: false,
+    //       list: res.data.sort(
+    //         (e1, e2) => new Date(e2.date) - new Date(e1.date)
+    //       ),
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     //TODO catch backend errors
+    //     console.log(err);
+    //     setExpenses({ ...expenses, loading: false });
+    //   });
     return expenses.list;
   };
 
